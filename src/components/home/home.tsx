2de5bad4781af55431f0bd6e4ed6d2a4afcd49cc
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-06-24 13:43:38 +0200

    Use authentication api in home and consolidate styling

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
 import './home.scss';
+import { authenticate } from '../../api/authentication';
 
 export class Home extends React.Component<{}, {}> {
-
-  authenticate() {
-    const clientId = 'b555c04661b94caabdbf19cdc57170b5';
-    const responseType = 'token';
-    const redirectUri = 'http://localhost:3000/authenticate';
-    const state = Math.round(Math.random() * 1000000);
-    const scopes = 'user-read-private'
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&state=${state}&show_dialog=false&scopes=${scopes}`;
-  }
 
   render() {
     return <div className="content">
@@ -22,7 +14,7 @@ export class Home extends React.Component<{}, {}> {
         <p>By clicking the button below, you will be redirected to Spotify, there you need to login and authorize the app for the mentioned permissions.</p>
       </section>
       <section className="authorization">
-        <button onClick={() => this.authenticate()}>Click here to authenticate Spotify</button>
+        <button onClick={() => authenticate()}>Click here to authenticate Spotify</button>
       </section>      
       <nav>
         <a href="/">Home</a> |
