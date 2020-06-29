8a48fa55f1c1ebdd4e472e17d7766f53ececb95c
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-06-29 21:23:15 +0200

    Switch from mock data to real data

--- src/store/recommendations/actions.ts
+++ src/store/recommendations/actions.ts
@@ -19,10 +19,10 @@ export function populateGenreSeeds(seeds: Array<string>): RecommendationActionTy
 export function fetchAndPopulateGenreSeeds(): ThunkAction<void, RootState, unknown, Action<Array<string>>> {
   return async function(dispatch: any, getState: any) {
     try {
-      // const response = await getAvailableGenreSeeds(getState().authentication.access_token);
-      // const data = await response.json();
+      const response = await getAvailableGenreSeeds(getState().authentication.access_token);
+      const data = await response.json();
 
-      const data = await JSON.parse(mockGenreSeeds);
+      // const data = await JSON.parse(mockGenreSeeds);
 
       return dispatch(populateGenreSeeds(data.genres));
     }
@@ -42,12 +42,10 @@ export function searchItems(artists: Array<Artist>, tracks: Array<Track>): Recom
 export function fetchSearchItems(query: string, types: Array<SearchTypes>): ThunkAction<void, RootState, unknown, Action<Array<string>>> {
   return async function(dispatch: any, getState: any) {
     try {
-      // const response = await getSearchItems(getState().authentication.access_token, query, types);
-      // const data = await response.json();
+      const response = await getSearchItems(getState().authentication.access_token, query, types);
+      const data = await response.json();
 
-      const data = await JSON.parse(mockResult);
-
-      console.log(data)
+      // const data = await JSON.parse(mockResult);
 
       return dispatch(searchItems(
         data.artists.items,
